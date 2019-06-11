d545b4aee798ca1690af77e85b504bec88182a32
neethu-ms <msneethu2989@gmail.com>
2019-06-11 19:30:23 -0400

    Stream example

---
+++ StreamExample.java
@@ -0,0 +1,15 @@
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class StreamExample {
+	
+	
+	public static void main(String[] args) {
+		
+		Stream<Integer> stream = Stream.of(90,4,9,8,8,6);
+		Optional<Integer> anyNumber = stream.findFirst();
+		anyNumber.ifPresent(System.out::println);
+		
+	}
+
+}
